9fe92f2ede75d1cf6fd94cc158aa574e4f9406cd
Wouter Mellema <wouter1mellema@gmail.com>
2020-01-14 01:25:01 +0100

    Updated Testing
    Added another testcase. TestSetup

--- tests.py
+++ tests.py
@@ -1,37 +1,55 @@
 import unittest, os, shutil,platform
 from pysteamcmdwrapper import *
 
+inst_dir = "tmp_installation"
 
 class TestInstallation(unittest.TestCase):
     def testCorrectDir(self):
         try:
-            os.mkdir('dir')
-            s = SteamCMD("dir")
+            if not os.path.isdir(inst_dir):
+                os.mkdir(inst_dir)
+            s = SteamCMD(inst_dir)
             assert s is not None
         except Exception as e:
-            if os.path.isdir('dir'):
-                shutil.rmtree('dir',ignore_errors=True)
+            if os.path.isdir(inst_dir):
+                shutil.rmtree(inst_dir,ignore_errors=True)
             raise e
-        shutil.rmtree('dir',ignore_errors=True)
+        finally:
+            shutil.rmtree(inst_dir,ignore_errors=True)
     def testIncorrectDir(self):
         self.assertRaises(SteamCMDInstallException,SteamCMD,installation_path="dir1")
 
     def testSetup(self):
-        pass
-    def testGameDownload(self):
         try:
-            os.mkdir('dir')
-            s = SteamCMD("dir")
+            if not os.path.isdir(inst_dir):
+                os.mkdir(inst_dir)
+            s = SteamCMD(inst_dir)
             assert s is not None
             s.install()
-            s.app_update(233780,validate=True)
-            assert os.path.isfile('dir/steamapps/common/Arma 3 Server/mpmissions/readme.txt')
+            assert os.path.isdir(inst_dir+'/package')
         except Exception as e:
-            if os.path.isdir('dir'):
-                pass
-                shutil.rmtree('dir',ignore_errors=True)
+            if os.path.isdir(inst_dir):
+                shutil.rmtree(inst_dir,ignore_errors=True)
             raise e
-        shutil.rmtree('dir',ignore_errors=True)
+        finally:
+            shutil.rmtree(inst_dir,ignore_errors=True)
+
+    def testGameDownload(self):
+        try:
+            if not os.path.isdir(inst_dir):
+                os.mkdir(inst_dir)
+            s = SteamCMD(inst_dir)
+            assert s is not None
+            s.install()
+            s.app_update(17575,validate=True)
+            # return
+            assert os.path.isdir(inst_dir+'/steamapps/common/Pirates, Vikings, and Knights II Dedicated Server/hl2')
+        except Exception as e:
+            if os.path.isdir(inst_dir):
+                shutil.rmtree(inst_dir,ignore_errors=True)
+            raise e
+        finally:
+            shutil.rmtree(inst_dir,ignore_errors=True)
 
 if __name__ == '__main__':
     unittest.main()
