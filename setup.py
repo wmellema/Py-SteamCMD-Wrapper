91fcd0e934da1def105bf78d7d6c882a6906ed7c
Wouter Mellema <w.mellema@iwink.nl>
2021-05-30 00:41:53 +0200

    Increase release version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py-steamcmd-wrapper",  # Replace with your own username
-    version="0.0.4",
+    version="0.0.5",
     author="Wouter Mellema",
     author_email="info@woutermellema.nl",
     description="Python wrapper for SteamCMD",
