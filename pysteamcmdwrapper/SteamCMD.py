7fa9ee6c1bf3f4407077455ec39773e2de1d52e8
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-05 17:24:53 +0000

    Adding tarfile member sanitization to extractall()

--- pysteamcmdwrapper/SteamCMD.py
+++ pysteamcmdwrapper/SteamCMD.py
@@ -91,7 +91,26 @@ class SteamCMD:
         elif self.platform == 'Linux':
             import tarfile
             with tarfile.open(self.zip, 'r:gz') as f:
-                f.extractall(self._installation_path)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner) 
+                    
+                
+                safe_extract(f, self._installation_path)
 
         else:
             # This should never happen, but let's just throw it just in case.
